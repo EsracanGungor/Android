bd3709ec18aeebf2b1d249b141b1afc4d10b7843
EsracanGungor <esracan.gungor@ogr.iu.edu.tr>
2019-09-07 14:16:56 +0300

    Android

---
+++ SurvivorBird/android/src/com/esragungor/survivorbird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.esragungor.survivorbird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.esragungor.survivorbird.SurvivorBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SurvivorBird(), config);
+	}
+}
